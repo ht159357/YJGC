85fa9b184b96e28a02c5a1253546bddb4c72280c
e <392135232@qq.com>
2017-11-15 10:07:40 +0800

    project-details ------Page

--- src/components/project-details.vue
+++ src/components/project-details.vue
@@ -28,12 +28,25 @@
         </div>
       </div>
       <div class="pro-price-right">
-        2
+        <div class="pro-price">
+          <div class="pro-price-num">&yen;398999.1</div>
+          <div class="pro-price-title">价格</div>
+        </div>
       </div>
     </div>
 
+    <div class="pro-options" v-for="item in 5">
+      <div class="pro-options-title">
+        是否卸甲服务 （简单30分钟0.01元，日系60分钟0.01元）
+      </div>
+      <div class="pro-options-box">
+        <div @click="btnSwitch($event)" class="pro-options-btn options-btn-active">简单卸甲</div>
+        <div @click="btnSwitch($event)" class="pro-options-btn">日系卸甲</div>
+        <div @click="btnSwitch($event)" class="pro-options-btn">否</div>
+      </div>
+    </div>
     <div class="pro-details">
-      --------------------------
+      <img class="pro-details-img" src="./../assets/img/attention.jpg" alt="">
     </div>
   </div>
 </template>
@@ -43,6 +56,20 @@
         data(){
             return {
 
+            }
+        },
+        methods:{
+            btnSwitch(event){
+                let self = event.target;
+                let parentEle = event.target.parentNode;
+                let childEles = parentEle.getElementsByClassName("pro-options-btn");
+                for(let i=0;i<childEles.length;i++){
+                    if( self == childEles[i] ){
+                        childEles[i].classList.add("options-btn-active");
+                    }else if( self !== childEles[i] ){
+                      childEles[i].classList.remove("options-btn-active");
+                    }
+                }
             }
         }
     }
@@ -87,8 +114,101 @@
   .pro-time .pro-title:first-child{
     margin-left: 0;
   }
+  .pro-options{
+    background: #fff;
+    padding: 10px 10px 0;
+  }
+  .pro-price{
+    width: 90%;
+    max-width: 90px;
+    margin: 0 auto;
+    border: 1px solid #E71B4F;
+    border-radius: 4px;
+    text-align: center;
+    position: relative;
+  }
+  .pro-price:before{
+    content: '';
+    width: 5px;
+    height: 5px;
+    background: #E71B4F;
+    border-radius: 50%;
+    position: absolute;
+    top: 5%;
+    left: 5%;
+  }
+  .pro-price:after{
+    content: '';
+    width: 5px;
+    height: 5px;
+    background: #E71B4F;
+    border-radius: 50%;
+    position: absolute;
+    top: 5%;
+    right: 5%;
+  }
+  .pro-price-num{
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    border-radius: 4px;
+    padding: 14px 0 8px;
+    background: #FEE2E7;
+    font-size: 18px;
+    font-weight: bold;
+    color: #E71B4F;
+    border-bottom: 1px solid #E71B4F;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+  .pro-price-title{
+    border-radius: 4px;
+    padding: 2px 0;
+    font-size: 18px;
+    color: #E71B4F;
+  }
+  .pro-options-title{
+    text-align: left;
+    position: relative;
+    font-size: 14px;
+    text-indent: 20px;
+  }
+  .pro-options-title:before{
+    content: "";
+    width: 10px;
+    height: 10px;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    border: 2px solid rgb(252,51,83);
+    background: #fff;
+    border-radius: 50%;
+  }
+  .pro-options-box{
+    text-align: left;
+  }
+  .pro-options-btn{
+    display: inline-block;
+    color: #5D5C5C;
+    margin-left: 35px;
+    height: 30px;
+    margin: 10px 0 0 35px;
+    line-height: 30px;
+    text-align: center;
+    width: 75px;
+    border-radius: 5px;
+    border: 1px solid #b1b1b1;
+  }
+  .options-btn-active{
+    background: #FC3353;
+    color: #fff;
+    border: 1px solid rgb(252,51,83);
+  }
   .pro-details{
     background: #fff;
-    margin-top: 10px;
+    padding-top: 30px;
+  }
+  .pro-details-img{
+    width: 100%;
   }
 </style>
