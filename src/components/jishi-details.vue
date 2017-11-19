6a656f0af628c83c1a05124ea53a0588eae52770
SunnyHaroro <392135232@qq.com>
2017-11-19 18:23:10 +0800

    增加无缝滚动

--- src/components/jishi-details.vue
+++ src/components/jishi-details.vue
@@ -248,7 +248,6 @@ export default {
     store,
     data () {
         return {
-            pagename:"showJsDetails",
             tabInfo:[
               {title:"在线选款",url:require("./../assets/img/xuanku1.png"),urlActive:require("./../assets/img/xuanku.png"),active:true,activeNum:0},
               {title:"在线预约",url:require("./../assets/img/allp1.png"),urlActive:require("./../assets/img/allp.png"),active:false,activeNum:1},
@@ -293,14 +292,14 @@ export default {
         loadMore() {
             this.loading = true;
             setTimeout(() => {
-              this.list.push('http://fuss10.elemecdn.com/b/18/0678e57cb1b226c04888e7f244c20jpeg.jpeg',
-                'http://fuss10.elemecdn.com/3/1e/42634e29812e6594c98a89e922c60jpeg.jpeg',
-                'http://fuss10.elemecdn.com/1/c5/95c37272d3e554317dcec1e17a9f5jpeg.jpeg',
-                'http://fuss10.elemecdn.com/7/85/e478e4b26af74f4539c79f31fde80jpeg.jpeg',
-                'http://fuss10.elemecdn.com/b/df/b630636b444346e38cef6c59f6457jpeg.jpeg',
-                'http://fuss10.elemecdn.com/7/a5/596ab03934612236f807b92906fd8jpeg.jpeg');
+                this.list.push('http://fuss10.elemecdn.com/b/18/0678e57cb1b226c04888e7f244c20jpeg.jpeg',
+                  'http://fuss10.elemecdn.com/3/1e/42634e29812e6594c98a89e922c60jpeg.jpeg',
+                  'http://fuss10.elemecdn.com/1/c5/95c37272d3e554317dcec1e17a9f5jpeg.jpeg',
+                  'http://fuss10.elemecdn.com/7/85/e478e4b26af74f4539c79f31fde80jpeg.jpeg',
+                  'http://fuss10.elemecdn.com/b/df/b630636b444346e38cef6c59f6457jpeg.jpeg',
+                  'http://fuss10.elemecdn.com/7/a5/596ab03934612236f807b92906fd8jpeg.jpeg');
                 this.loading = false;
-            }, 3000);
+            }, 2000);
         },
         scrollLeft(){
             var speed = 20;
@@ -533,23 +532,28 @@ export default {
   }
   .jis-pop-time-title{
     display: flex;
+    align-items: center;
     padding: 10px 0;
+    font-size: 14px;
   }
   .jis-pop-time-left{
     flex: 0 0 25%;
     font-weight: bold;
+    height: 21px;
+    line-height: 21px;
   }
   .jis-pop-time-right{
     flex: 0 0 75%;
     overflow: hidden;
     font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
   }
   .jis-pop-time-green,.jis-pop-time-gary{
     display: inline-block;
-    width: 20px;
-    height: 20px;
-    line-height: 20px;
-    vertical-align: middle;
+    width: 15px;
+    height: 15px;
     float: left;
   }
   .jis-pop-time-green{
@@ -562,8 +566,8 @@ export default {
   .jis-pop-time-name{
     margin-left: 5px;
     display: inline-block;
-    height: 20px;
-    line-height: 20px;
+    height: 21px;
+    line-height: 21px;
     vertical-align: middle;
     float: left;
   }
@@ -594,11 +598,10 @@ export default {
     width: 100%;
   }
   .jis-pop-show-more{
-    margin-right: 5px;
+    margin-right: 20px;
     display: inline-block;
-    height: 20px;
-    line-height: 20px;
-    vertical-align: middle;
+    height: 21px;
+    line-height: 21px;
     float: right;
     font-weight: normal;
   }
--- src/components/my-yanbi.vue
+++ src/components/my-yanbi.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="yb-app">
     <mt-cell class="yb-top-title" :title="'2017-09'" :value="'说明文字'"></mt-cell>
-    <mt-loadmore :top-method="loadTop" :bottom-method="loadBottom" :bottom-all-loaded="allLoaded" ref="loadmore" class="yb-list">
+    <mt-loadmore :top-method="loadTop" ref="loadmore" class="yb-list">
+      <ul v-infinite-scroll="loadMore"
+          infinite-scroll-disabled="loading"
+          infinite-scroll-distance="10">
+        <li v-for="item in list">
+          <div class="yb-one-list">
+            <div class="yb-img">
+              <img src="./../assets/img/yb-add.png">
+            </div>
+            <div class="yb-detail">
+              <span class="yb-detail-title">充值</span><br>
+              <span class="yb-detail-time">09/29 17:51</span>
+            </div>
+            <div class="yb-money">
+              +6699
+            </div>
+          </div>
+        </li>
+      </ul>
 
-      <div class="yb-one-list" v-for="item in 10">
-        <div class="yb-img">
-          <img src="./../assets/img/yb-add.png">
-        </div>
-        <div class="yb-detail">
-          <span class="yb-detail-title">充值</span><br>
-          <span class="yb-detail-time">09/29 17:51</span>
-        </div>
-        <div class="yb-money">
-          +6699
-        </div>
+      <div class="jis-loadmore" v-show="loading">
+        <mt-spinner color="#FB3453" type="triple-bounce"></mt-spinner>
       </div>
 
     </mt-loadmore>
@@ -22,27 +31,35 @@
 <script>
   import Vue from 'vue'
   import { Loadmore } from 'mint-ui';
+  import { InfiniteScroll } from 'mint-ui';
 
   Vue.component(Loadmore.name, Loadmore);
+  Vue.use(InfiniteScroll);
   export default {
       name:"my-yanbi",
       data(){
           return {
-              allLoaded:false,
+              list:[1,2,3,4,5,6,7,8,9,10],
+              loading: false,
           }
       },
       methods:{
           loadTop(){
+              let refersh= this.$refs.loadmore;
               setTimeout(function(){
                   // 加载更多数据
-                  this.$refs.loadmore.onTopLoaded();
+                  refersh.onTopLoaded();
               },1000)
           },
-          loadBottom(){
-
-            // 加载更多数据
-            this.allLoaded = true;// 若数据已全部获取完毕
-            this.$refs.loadmore.onBottomLoaded();
+          loadMore(){
+              this.loading = true;
+              setTimeout(() => {
+                  let last = this.list[this.list.length - 1];
+                  for (let i = 1; i <= 10; i++) {
+                      this.list.push( i );
+                  }
+                  this.loading = false;
+              }, 2000);
           }
       }
   }
@@ -92,5 +109,10 @@
     flex: 0 0 25%;
     font-size: 16px;
     font-weight: bold;
+  }
+  .jis-loadmore{
+    text-align: center;
+    width: 100%;
+    padding: 10px 0;
   }
 </style>
